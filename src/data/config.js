eb2962cf2c3f25e05523e8fb14714ff6a5519353
amangupta37 <amangupta6121998@gmail.com>
2021-01-23 14:19:15 +0530

    my q profile

---
+++ src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Aman Gupta",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Aman Gupta and I'm a Front-End Developer!",
+  githubUrl: "https://github.com/amangupta37",
+  linkedinUrl: "https://www.linkedin.com/in/aman-gupta-649168143/",
+  cvLink: "https://drive.google.com/file/d/1P_9Weg8Oq83In5qH0qNdGnk9zawi0UE6/view"
+}
